1a7cdea2e526474bdfaf3124b08c7df61261c53d
Shams-me <imomnazarovshamsyt@gmail.com>
2024-04-22 12:13:25 +0500

    Logging about waiting for postgres

--- etc/postgres/init_ddl.sh
+++ etc/postgres/init_ddl.sh
@@ -4,6 +4,7 @@ set -e
 
 while ! pg_isready -U $POSTGRES_USER -d $POSTGRES_DB; do
   sleep 1
+  echo "Waiting for postgres to be ready..."
 done
 
 psql -U $POSTGRES_USER -d $POSTGRES_DB -f /etc/app/database.ddl
